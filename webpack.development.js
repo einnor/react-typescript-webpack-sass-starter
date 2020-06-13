af5abb935cdf193ebe5fc227c4b7d7cecfb4c00e
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-06-13 09:58:16 +0300

    Development webpack config

---
+++ webpack.development.js
@@ -0,0 +1,15 @@
+const merge = require('webpack-merge');
+const path = require('path');
+
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true,
+    historyApiFallback: true,
+    port: 8888,
+  },
+});
