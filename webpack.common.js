42d5121a429d60228d88d51c91cae388cc259dfb
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-06-13 10:46:32 +0300

    Debugging

--- webpack.common.js
+++ webpack.common.js
@@ -3,6 +3,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
