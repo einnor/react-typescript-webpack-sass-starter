6140a891dfc477646fd3d7fb995cb0cd56122eef
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-06-13 10:54:58 +0300

    Configure sass to use module system

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom';
 import styles from './index.scss';
 
 const App = () => {
-  return <h1 className={styles.title}>Hello Wepack!</h1>;
+  return <h1 className={styles.title}>Hello Wepack!!!</h1>;
 };
 ReactDOM.render(<App />, document.getElementById('root'));
--- webpack.common.js
+++ webpack.common.js
@@ -32,7 +32,11 @@ module.exports = {
           // Creates `style` nodes from JS strings
           'style-loader',
           // Translates CSS into CommonJS
-          'css-loader',
+          // 'css-loader',
+          {
+            loader: 'css-loader',
+            options: { modules: true }
+          },
           // Compiles Sass to CSS
           'sass-loader',
         ],
